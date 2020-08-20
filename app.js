1c1d574d9728dca6f96080538d9aafbd3d0cf90f
Matt Werth <matthewwerth@gmail.com>
2020-08-20 09:44:08 -0400

    Installed Express

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-const a = "hi"
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      mongoose = require(‘mongoose’),
+      port = 3000
+
+mongoose.connect('mongodb://localhost:27017/db_name', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to DB!')).catch(error => console.log(error.message));
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+//Local Server
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
