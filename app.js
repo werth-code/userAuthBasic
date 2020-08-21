ef6a7511f4047907749761e317c3989702e36351
Matt Werth <matthewwerth@gmail.com>
2020-08-21 07:43:37 -0400

    Added Basic Authentication

--- app.js
+++ app.js
@@ -1,19 +1,83 @@
-const express = require('express'),
-      app = express(),
-      bodyParser = require('body-parser'),
-      mongoose = require(‘mongoose’),
-      port = 3000
+const express = require("express"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  bodyParser = require("body-parser"),
+  User = require("./models/user"),
+  LocalStrategy = require("passport-local"),
+  //passportLocalMongoose = require("passport-local-mongoose"),
+  port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/db_name', {
+
+
+mongoose.connect('mongodb://localhost:27017/authApp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('Connected to DB!')).catch(error => console.log(error.message));
 
+const app = express();
+
 app.set("view engine", "ejs")
-app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
+//app.use(express.static("public"));
+
+app.use(
+  require("express-session")({
+    secret: "Bear is the best and most purdy dog",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.get("/secret", (req, res) => {
+    res.render("secret")
+})
+
+//Auth Routes
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+app.post("/register", (req, res) => {
+    req.body.username
+    req.body.password
+    User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+        if(err) {
+            console.log(err)
+            return res.render("register")
+        } 
+        passport.authenticate("local")(req, res, () => {
+            res.redirect("/login")
+        })  
+    })
+})
+
+//Log In Routes
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.post("/login", passport.authenticate("local", {  //This is an example of middleware
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), (req, res) => {
+
+})
+
+//Log Out Routes
 
 //Local Server
 app.listen(port, () =>
